89a3c10c42a0a16148a071fa0fc4e7a98e4a340f
Martin-NB\Martin <fprt77@gmail.com>
2018-01-23 14:41:09 +0100

    login - Unit test

--- api/src/test/java/com/deluxe/sveapi/ApiManagerTest.java
+++ api/src/test/java/com/deluxe/sveapi/ApiManagerTest.java
@@ -1,6 +1,9 @@
 package com.deluxe.sveapi;
 
+import com.deluxe.sveapi.data.request.session.LoginRequest;
 import com.deluxe.sveapi.data.response.common.BaseResponse;
+import com.deluxe.sveapi.data.response.common.Resp;
+import com.deluxe.sveapi.data.response.session.SessionResponse;
 
 import org.junit.After;
 import org.junit.Before;
@@ -16,22 +19,7 @@ import static org.junit.Assert.fail;
 
 public class ApiManagerTest {
 
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
-
-    @Before
-    public void setUpStreams() {
-        System.setOut(new PrintStream(outContent));
-        System.setErr(new PrintStream(errContent));
-    }
-
-    @After
-    public void restoreStreams() {
-        System.setOut(System.out);
-        System.setErr(System.err);
-    }
-
-    public ApiManager prepareApiManager() {
+    private ApiManager prepareApiManager() {
 
         ApiManager apiManager = new ApiManager();
 
@@ -44,9 +32,22 @@ public class ApiManagerTest {
         return apiManager;
     }
 
+    private void assertErrorCodes(BaseResponse baseResponse) {
+        if (baseResponse.getRes()==null || baseResponse.getRes().getResp()==null || baseResponse.getRes().getResp().size()==0)
+            fail("Service call failed. Missing resp element(s)");
+        for (Resp resp : baseResponse.getRes().getResp()) {
+            if (resp.getCode().startsWith("err")) {
+                fail("Service call returned error. Response code: " + resp.getCode());
+            }
+            else {
+                System.out.println("Success isVersionSupported call. Response code: " + resp.getCode());
+            }
+        }
+    }
+
     @Test
     public void isVersionSupported() throws Exception {
-        System.out.print("isVersionSupported Test");
+        System.out.println("isVersionSupported Test\n");
         ApiManager apiManager = prepareApiManager();
         String deviceType = "m_AndroidPhoneSmall";
         String majorVersion = "4";
@@ -57,13 +58,7 @@ public class ApiManagerTest {
         try {
             retrofit2.Response<BaseResponse> response = call.execute();
             if (response.body()!=null) {
-                // success
-                try {
-                    System.out.print("Success isVersionSupported call. Response code: " + response.body().getRes().getResp().get(0).getCode());
-                }
-                catch (Exception e) {
-                    fail("Service call corrupted data object. Message: "+e.getMessage());
-                }
+                assertErrorCodes(response.body());
             }
             else {
                 try {
@@ -76,11 +71,38 @@ public class ApiManagerTest {
         catch (IOException e) {
             fail("Service call failed. Message: "+e.toString());
         }
+        System.out.println("\n\n");
     }
 
     @Test
     public void login() throws Exception {
+        System.out.println("login Test\n");
         ApiManager apiManager = prepareApiManager();
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.lang = "end";
+        loginRequest.login_info = new LoginRequest.LoginInfo();
+        loginRequest.login_info.d_id = "deviceid123456";
+        loginRequest.login_info.d_type = "m_AndroidPhoneSmall";
+        loginRequest.login_info.u_id = "unittest@sve.com";
+        loginRequest.login_info.pw = "unittest";
+        Call<SessionResponse> call = apiManager.login(loginRequest);
+        try {
+            retrofit2.Response<SessionResponse> response = call.execute();
+            if (response.body()!=null) {
+                assertErrorCodes(response.body());
+            }
+            else {
+                try {
+                    fail("Service call returned error. Message: "+response.errorBody().string());
+                } catch (IOException e) {
+                    fail("Service call returned error. Message could not be retrieved. Code: "+response.code());
+                }
+            }
+        }
+        catch (IOException e) {
+            fail("Service call failed. Message: "+e.toString());
+        }
+        System.out.println("\n\n");
     }
 
 }
